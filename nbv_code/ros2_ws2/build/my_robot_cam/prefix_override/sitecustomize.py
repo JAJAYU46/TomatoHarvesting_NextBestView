ab61324e08e3d6b4bbb3f3ef93eca95c203c58e1
JAJAYU46 <jajajaja4646@gmail.com>
2025-02-28 00:26:06 +0800

    Confirm the right orientation calculation and adjust NBV BestViewModel & SDF parameters

--- nbv_code/ros2_ws2/build/my_robot_cam/prefix_override/sitecustomize.py
+++ nbv_code/ros2_ws2/build/my_robot_cam/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/jajayu/TomatoHarvesting_NextBestView/nbv_code/ros2_ws2/install/my_robot_cam'
+    sys.prefix = sys.exec_prefix = '/home/rmml05/nbv_JaJaYu/TomatoHarvesting_NextBestView/nbv_code/ros2_ws2/install/my_robot_cam'
--- nbv_code/ros2_ws2/install/setup.ps1
+++ nbv_code/ros2_ws2/install/setup.ps1
@@ -23,7 +23,12 @@ function _colcon_prefix_chain_powershell_source_script {
 
 # source chained prefixes
 _colcon_prefix_chain_powershell_source_script "/opt/ros/humble\local_setup.ps1"
-_colcon_prefix_chain_powershell_source_script "/home/jajayu/ros2_ws/install\local_setup.ps1"
+_colcon_prefix_chain_powershell_source_script "/home/rmml05/thr_ws/install\local_setup.ps1"
+_colcon_prefix_chain_powershell_source_script "/home/rmml05/micro_ros_ws/install\local_setup.ps1"
+_colcon_prefix_chain_powershell_source_script "/home/rmml05/ws_moveit/install\local_setup.ps1"
+_colcon_prefix_chain_powershell_source_script "/home/rmml05/thr_ws_release/install\local_setup.ps1"
+_colcon_prefix_chain_powershell_source_script "/home/rmml05/ros-humble-ros1-bridge/install\local_setup.ps1"
+_colcon_prefix_chain_powershell_source_script "/home/rmml05/custom_msgs/custom_msgs_ros2/install\local_setup.ps1"
 
 # source this prefix
 $env:COLCON_CURRENT_PREFIX=(Split-Path $PSCommandPath -Parent)
